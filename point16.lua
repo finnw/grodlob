915185d584a20f0b08e2dff0d4db1798956f545e
finnw <finnw@firecloudsw.co.uk>
2013-10-20 21:38:22 +0100

    zlib ffi bindings from lua-files
    Modified to use 'ffilib' lookup

--- point16.lua
+++ point16.lua
@@ -1,16 +1,19 @@
 local ffi = require 'ffi'
 
+local ffi_copy = ffi.copy
+local ffi_string = ffi.string
+
 local point16 = {}
 
 local buffer = ffi.new "struct {int16_t x, y;}"
 
 function point16.fromXY(x, y)
   buffer.x, buffer.y = x, y
-  return ffi.string(buffer, 4)
+  return ffi_string(buffer, 4)
 end
 
 function point16.toXY(p)
-  ffi.copy(buffer, p, 4)
+  ffi_copy(buffer, p, 4)
   return buffer.x, buffer.y
 end
 
